1b95e5e09e8d537dcebcb7852b1190fba063fcfe
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-05-24 20:24:33 +0600

    Time: 142 ms (46.96%), Space: 18.7 MB (67.46%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,9 +2,15 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         m = Counter(nums)
         ans = []
-        for x,y in m.most_common(k):
-                ans.append(x)
-               
+        h = []
+        for key,val in m.items(): # loop takes O(n) time  # push takes O(logn) time # so TC: O(nlogn)
+            heappush(h,(-val, key))  # by default python heap is minheap. by multiplying -1 to the val we                                        #make this heap a max heap
+                                   
+        count = 0
+        while count<k:
+            ans.append(heappop(h)[1]) # O(klogk) #we take only item not frequency
+            count += 1
+            
         return ans
                 
                      
