f4f3c2f24ba33d545e41a1489270162332e4649f
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-05-28 01:18:55 +0600

    Time: 100 ms (85.55%), Space: 13.9 MB (82.58%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,30 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
+        # validation check of row and column
         for i in range(len(board)):
-            s = set()
+            s1 = set()
+            s2 = set()
             for j in range(len(board)):
-                if board[i][j]  in s:
-                    return False
-                if board[i][j] != "." and board[i][j]  not in s:
-                    s.add(board[i][j])
+                if board[i][j] != ".":   #row check
+                    if board[i][j] in s1 :
+                        return False
+                    s1.add(board[i][j])
+                if board[j][i] != ".":  # column check
+                    if board[j][i] in s2:
+                        return False
+                    s2.add(board[j][i])
                 
-        for i in range(len(board)):
-            s = set()
-            for j in range(len(board)):
-                if board[j][i]  in s:
-                    return False
-                if board[j][i] != "." and board[j][i]  not in s:
-                    s.add(board[j][i])
-                
+        # 3*3 grid  check        
         for i in range(0,len(board),3):
             for j in range(0,len(board),3):
-                s = set()
-                for k in range(i, i+3,1):
+                s = set()                  # selecting per grid out of 9 by outer 2 loop
+                for k in range(i, i+3,1):  # below 2 loop is used forlooping through per grid(3*3 matrix)
                     for l in range(j, j+3,1):
                         if  board[k][l]  in s:
                             return False
-                        if board[k][l] != "." and board[k][l]  not in s:
+                        if board[k][l] != "." :
                             s.add(board[k][l])
                         
                         
