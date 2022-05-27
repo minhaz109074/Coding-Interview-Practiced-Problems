b76d8a83212f9ceb2a20eff99af5817d2d542805
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-05-27 11:46:34 +0600

    Time: 279 ms (60.35%), Space: 21 MB (81.32%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,13 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        ans = []
+        n = len(nums)
+        ans = [1]*n
         left, right = 1, 1
-        for i in range(len(nums)):
-            ans.append(left)
+        # instead of using two separate loop for left and right value calculation, we just did it
+        # in one single pass by using another pointer n-1-i from reverse for right values
+        for i in range(n):
+            ans[i] *= left
             left = left*nums[i]
-        for j in reversed(range(len(nums))):
-            ans[j] *= right
-            right = right*nums[j]
-        
+            ans[n-1-i] *= right     
+            right = right*nums[n-1-i]
+
         return ans
