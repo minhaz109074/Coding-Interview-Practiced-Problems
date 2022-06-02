c2c675cc463ee49e7dc44566eae680683151a106
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-06-02 10:56:17 +0600

    Time: 817 ms (79.65%), Space: 17.2 MB (91.15%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -4,6 +4,8 @@ class Solution:
         s = set()
         nums.sort()
         for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             l = i+1
             r = n-1
             while(l<r):
