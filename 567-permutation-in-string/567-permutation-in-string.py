12c90a86267826110bfeb1c3395c2ee7ac6ab837
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-06-05 22:43:37 +0600

    Time: 140 ms (44.98%), Space: 14 MB (30.77%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,14 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        #TC: O(n*m) -> m = size of s1 string
         s1_count = Counter(s1)
-        beg, end = 0, len(s1)
-        while(end<=len(s2)):
-            window = s2[beg:end]
-            if Counter(window) == s1_count:
-                return True
-            else:
-                beg += 1
+        beg, end = 0, 0
+        mp = defaultdict(int)
+        count = 0
+        while(end<len(s2)):
+            mp[s2[end]] += 1
+            if  count == len(s1)-1:
+                if mp == s1_count:
+                    return True
+                else:
+                    mp[s2[beg]] -= 1
+                    count -= 1
+                    if mp[s2[beg]] == 0:
+                        del mp[s2[beg]]
+                    beg += 1
             end += 1
+            count += 1
             
         return False
         
