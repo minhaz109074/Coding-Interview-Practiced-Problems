5c2f75370ff0d7e491801f3ce5faf6ce9170766c
Minhazul Abedin <67797374+minhaz109074@users.noreply.github.com>
2022-06-09 11:46:22 +0600

    Time: 1715 ms (38.65%), Space: 25 MB (58.24%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -3,12 +3,11 @@ class Solution:
         n = len(temperatures)
         ans = [0]*n
         stack = []
-        stack.append((temperatures[0], 0))
-        for i in range(1, n):
-            while stack and stack[-1][0]<temperatures[i]:
-                ans[stack[-1][1]] = i - stack[-1][1]
+        for i in range(n):
+            while stack and temperatures[stack[-1]]<temperatures[i]:
+                ans[stack[-1]] = i - stack[-1]
                 stack.pop()
-            stack.append((temperatures[i],i))
+            stack.append(i)
             
         return ans
 
